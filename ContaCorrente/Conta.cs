590947387fc4a8e45963a10e6342aebfa8b8f010
kelvin <kelvin@PROTECH-SSC6>
2021-11-29 10:40:33 -0300

    Add project files.

---
+++ ContaCorrente/Conta.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaCorrente
+{
+    public class Conta
+    {
+
+        public Cliente Titular { get; set; }
+
+        
+        public static int totalContascriadas { get; private set; }
+        
+        public static double taxaOperacao { get; private set;}
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        public double taxaOpp { get; set; }
+        private double _saldo = 100;
+
+        public Conta(int agencia , int numero)
+        {
+            this.Agencia = agencia;
+            this.Numero = numero;
+            taxaOperacao = 30 / totalContascriadas;
+            totalContascriadas++;
+        }
+        public double getTaxaOperacao()
+        {
+            taxaOpp = taxaOperacao;
+            return taxaOpp;
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+
+        public bool Transferir(double valor, Conta contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ ContaCorrente/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ContaCorrente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta(123, 1234);
+            Console.WriteLine(conta.taxaOpp);
+
+            Console.ReadLine();
+        }
+    }
+}
